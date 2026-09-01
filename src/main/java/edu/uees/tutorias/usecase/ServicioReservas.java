e6f9bdcee6c04eaa2675b9f01cdc541ca1f09275
Gonzalo Guzmán <gonzalo.guzman@uees.edu.ec>
2026-09-01 16:52:28 -0500

    feat: implementar factory method

--- src/main/java/edu/uees/tutorias/usecase/ServicioReservas.java
+++ src/main/java/edu/uees/tutorias/usecase/ServicioReservas.java
@@ -3,23 +3,32 @@ package edu.uees.tutorias.usecase;
 import edu.uees.tutorias.domain.Estudiante;
 import edu.uees.tutorias.domain.HorarioTutoria;
 import edu.uees.tutorias.domain.Reserva;
+import edu.uees.tutorias.factory.EmailCreator;
 import edu.uees.tutorias.factory.Notificador;
+import edu.uees.tutorias.factory.NotificadorCreator;
+import edu.uees.tutorias.factory.WhatsAppCreator;
 
 public class ServicioReservas {
     private final RepositorioReservas repositorioReservas;
-    private final Notificador notificador;
 
-    public ServicioReservas(RepositorioReservas repositorioReservas, Notificador notificador) {
+    public ServicioReservas(RepositorioReservas repositorioReservas) {
         this.repositorioReservas = repositorioReservas;
-        this.notificador = notificador;
     }
 
     public Reserva crearReserva(String idReserva, Estudiante estudiante, HorarioTutoria horario) {
         Reserva nuevaReserva = new Reserva(idReserva, estudiante, horario);
         repositorioReservas.guardar(nuevaReserva);
 
-        notificador.enviarNotificacion(
+        NotificadorCreator emailCreator = new EmailCreator();
+        emailCreator.notificar(
                 horario.getDocente().getCorreo(),
+                "Nueva Tutoría Solicitada",
+                "El estudiante " + estudiante.getNombre() + " ha reservado el horario."
+        );
+
+        NotificadorCreator whatsAppCreator = new WhatsAppCreator();
+        whatsAppCreator.notificar(
+                horario.getDocente().getCelular(),
                 "Nueva Tutoría Solicitada",
                 "El estudiante " + estudiante.getNombre() + " ha reservado el horario."
         );
@@ -34,8 +43,16 @@ public class ServicioReservas {
         reserva.cancelar();
         repositorioReservas.actualizar(reserva);
 
-        notificador.enviarNotificacion(
+        NotificadorCreator emailCreator = new EmailCreator();
+        emailCreator.notificar(
                 reserva.getHorario().getDocente().getCorreo(),
+                "Tutoría Cancelada",
+                "La reserva del estudiante " + reserva.getEstudiante().getNombre() + " ha sido cancelada."
+        );
+
+        NotificadorCreator whatsAppCreator = new WhatsAppCreator();
+        whatsAppCreator.notificar(
+                reserva.getHorario().getDocente().getCelular(),
                 "Tutoría Cancelada",
                 "La reserva del estudiante " + reserva.getEstudiante().getNombre() + " ha sido cancelada."
         );
