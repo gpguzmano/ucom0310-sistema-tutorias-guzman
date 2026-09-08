4c83c152bdfedd19c96d1d67f220fc5abc7a856e
Gonzalo Guzmán <gonzalo.guzman@uees.edu.ec>
2026-09-08 12:46:21 -0500

    fix: correción de los constructores estudiantes y docentes, incluir atributo celular y pasarlo al constructor de la clase abstracta de usuario

--- src/main/java/edu/uees/tutorias/domain/Docente.java
+++ src/main/java/edu/uees/tutorias/domain/Docente.java
@@ -3,8 +3,8 @@ package edu.uees.tutorias.domain;
 public class Docente extends Usuario {
     private final String departamento;
 
-    public Docente(String id, String nombres, String email, String departamento) {
-        super(id, nombres, email);
+    public Docente(String id, String nombres, String email, String celular, String departamento) {
+        super(id, nombres, email, celular);
 
         if (departamento == null || departamento.isBlank()) {
             throw new IllegalArgumentException("El departamento es obligatorio.");
--- src/main/java/edu/uees/tutorias/domain/Estudiante.java
+++ src/main/java/edu/uees/tutorias/domain/Estudiante.java
@@ -3,8 +3,8 @@ package edu.uees.tutorias.domain;
 public class Estudiante extends Usuario {
     private final String matricula;
 
-    public Estudiante(String id, String nombres, String email, String matricula) {
-        super(id, nombres, email);
+    public Estudiante(String id, String nombres, String email, String celular, String matricula) {
+        super(id, nombres, email, celular);
 
         if (matricula == null || matricula.isBlank()) {
             throw new IllegalArgumentException("La matricula es obligatoria.");
